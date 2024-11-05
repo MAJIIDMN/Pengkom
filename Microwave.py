56e91e3cfe1a5da9ad5ef9095aafe07e155dceae
Muhammad Nur Majiid <86580622+MAJIIDMN@users.noreply.github.com>
2024-11-05 23:51:40 +0700

    Update Microwave.py

--- Microwave.py
+++ Microwave.py
@@ -1,3 +1,7 @@
+import time
+import os
+import csv
+
 class comp:
     def __init__(self, time:int, temp:int, moisture:int):
         self.time = time*60
@@ -5,11 +9,12 @@ class comp:
         self.moisture = moisture
     def moisture1(self, limit):
         if self.moisture >= limit:
-            while self.time > 0:
+            time = self.time
+            while time > 0:
                 self.moisture = self.moisture - (self.temp/1200)
                 if self.moisture <= limit:
                     break
-                self.time -= 1
+                time -= 1     
 
 class mode(comp):
     def __init__(self, time, temp, moisture):
@@ -31,6 +36,7 @@ class vegetable(comp):
     def __init__(self, time, temp, moisture):
         super().__init__(time, temp, moisture)
     def menu():
+        Microwave.display1()
         print(" 1. brokoli")
         print(" 2. wortel\n 3. kembang kol\n 4. bayam")
         print(" 5. jagung manis\n 6. paprika")
@@ -68,6 +74,7 @@ class seafood(comp):
     def __init__(self, time, temp, moisture):
         super().__init__(time, temp, moisture)
     def menu():
+        Microwave.display1()
         print(" 1. Ikan fillet")
         print(" 2. Udang")
         print(" 3. Cumi-cumi")
@@ -93,6 +100,41 @@ class seafood(comp):
         comp.moisture1(self, 70)
         Microwave.display2(self)
         return print(f'Selamat menikmati!!')
+
+class incode(comp):
+    def __init__(self, time:int, temp:int, moisture:int, barcode:int, namefood):
+        super().__init__(time, temp, moisture)
+        self.barcode = barcode
+        namefood = namefood
+    def detect(self, code):
+        csvfile = open('barcodes.csv', newline='')
+        foodcode = csv.reader(csvfile)
+        next(foodcode)
+        barcode = []
+        namefood = []
+        timefood = []
+        tempfood = []
+        moisturefood = []
+        for row in foodcode:
+            barcode.append(int(row[1]))
+            namefood.append(row[2])
+            timefood.append(int(row[3])*60)
+            tempfood.append(int(row[4]))
+            moisturefood.append(int(row[5]))
+        csvfile.close()
+        for i in range(len(barcode)):
+            if code == barcode[i]:
+                self.namefood = namefood[i]
+                self.barcode = barcode[i]
+                self.time = timefood[i]
+                self.temp = tempfood[i]
+                self.moisture = moisturefood[i]
+                break               
+    def cook(self):
+        comp.moisture1(self, 0)
+        Microwave.display2(self)
+        return print(f'Selamat menikmati!!')
+
 class Microwave(comp):
     def __init__(self, time, temp, moisture):
         super().__init__(time, temp, moisture)
@@ -131,60 +173,68 @@ class Microwave(comp):
             side_h-=1
         print("="*(22))
     def display2(self):
-        side_h = 2
-        print("="*(22))
-        while side_h>=0:
-            side_v = 20
-            while side_v>0:
-                if side_h == 2:
-                    if self.temp<10:
-                        space = 12
-                    elif self.temp <100:
-                        space = 11
-                    else:
-                        space = 10
-                        
-                    if side_v == 20 or side_v == 2:
-                        print("||", end='')
-                    elif side_v == 1:
-                        print(' ')
-                    elif side_v == 19:
-                        print(f'Suhu: {self.temp}°C', end='')
-                    elif side_v<space:
-                        print(' ', end='')
-                elif side_h == 1:
-                    if self.moisture<10:
-                        space = 7
-                    elif self.moisture <100:
-                        space = 6
-                    else:
-                        space = 5
-                        
-                    if side_v == 20 or side_v == 2:
-                        print("||", end='')
-                    elif side_v == 1:
-                        print(' ')
-                    elif side_v == 19:
-                        print(f'Kelembapan: {int(round(self.moisture, 0))}%', end='')
-                    elif side_v<space:
-                        print(' ', end='') 
-                elif side_h == 0:
-                    if self.time<600:
-                        space = 5
-                    else:
-                        space = 4
-                        
-                    if side_v == 20 or side_v == 2:
-                        print("||", end='')
-                    elif side_v == 1:
-                        print(' ')
-                    elif side_v == 19:
-                        print(f'Sisa Waktu: {int((self.time/60))}:00', end='')
-                    elif side_v<space:
-                        print(' ', end='')   
-                side_v-=1
-            side_h-=1
-        print("="*(22))
+        while self.time >= 0:
+            os.system('cls')
+            side_h = 2
+            print("="*(22))
+            while side_h>=0:
+                side_v = 20
+                while side_v>0:
+                    if side_h == 2:
+                        if self.temp<10:
+                            space = 12
+                        elif self.temp <100:
+                            space = 11
+                        elif self.temp <1000:
+                            space = 10
+                        else:
+                            space = 9
+                            
+                        if side_v == 20 or side_v == 2:
+                            print("||", end='')
+                        elif side_v == 1:
+                            print(' ')
+                        elif side_v == 19:
+                            print(f'Suhu: {self.temp}°C', end='')
+                        elif side_v<space:
+                            print(' ', end='')
+                    elif side_h == 1:
+                        moisture = self.moisture + (self.temp/1500)*self.time
+                        if moisture<=9:
+                            space = 7
+                        elif moisture <=99:
+                            space = 6
+                        else:
+                            space = 5
+                            
+                        if side_v == 20 or side_v == 2:
+                            print("||", end='')
+                        elif side_v == 1:
+                            print(' ')
+                        elif side_v == 19:
+                            print(f'Kelembapan: {int(round(moisture, 0))}%', end='')
+                        elif side_v<space:
+                            print(' ', end='') 
+                    elif side_h == 0:
+                        if self.time<6000:
+                            space = 4
+                        else:
+                            space = 3
+                            
+                        if side_v == 20 or side_v == 2:
+                            print("||", end='')
+                        elif side_v == 1:
+                            print(' ')
+                        elif side_v == 19:
+                            min, sec = divmod(self.time, 60)
+                            print(f"Sisa Waktu: {min:02d}:{sec:02d}", end='')
+                        elif side_v<space:
+                            print(' ', end='')   
+                    side_v-=1
+                side_h-=1
+            print("="*(22))
+            time.sleep(0.0001) 
+            self.time -= 1
     def displayover():
         side_h = 2
         print("="*(22))
@@ -197,7 +247,7 @@ class Microwave(comp):
                     elif side_v == 1:
                         print(' ')
                     elif side_v == 14:
-                        print("Overheat",end='')
+                        print("Overcook",end='')
                     elif (side_v<20 and side_v>14) or side_v<8:
                         print(' ', end='')
                 else:
@@ -236,10 +286,10 @@ class Microwave(comp):
             side_h-=1
         print("="*(22))
     def menu():
-        print(" 1. Start")
+        print(" 1. Manual mode")
         print(" 2. Quick Heat\n 3. Deforst\n 4. Grill")
         print(" 5. Vegetable\n 6. Seafood")
-        print(" 7. Cleaning")
+        print(" 7. input kode\n 8. Cleaning")
         print(" 0. Stop")
         print(" 99. Child Lock")
     def Start():
@@ -247,18 +297,20 @@ class Microwave(comp):
         while True:
             Microwave.menu()
             chioce = int(input("Pilih menu : "))
+            os.system('cls')
             
             if chioce == 1:
+                Microwave.display1()
                 time = int(input("Masukan waktu Memasak(menit) : "))
                 temp = int(input("Masukan Temperatur(°C) : "))
                 moisture = 100
                 food = Microwave(time, temp, moisture)
                 food.moisture1(0)
+                food.display2()
                 if food.moisture < 1:
                     Microwave.displaydry()
                 elif food.moisture < 40:
                     Microwave.displayover()
-                food.display2()
             elif chioce == 2:
                 food = mode(2, 150, 90)
                 food.quick_heat()
@@ -266,30 +318,31 @@ class Microwave(comp):
                 food = mode(8, 40, 90)
                 food.Deforst()
             elif chioce == 4:
-                food = mode(20, 230, 100)
+                food = mode(7, 230, 100)
                 food.Grill()
             elif chioce == 5:
                 while True:
                     vegetable.menu()
                     sayur = int(input("Pilih Sayur : "))
+                    os.system('cls')
                     if sayur == 1:
                         vegen = vegetable(2, 200, 100)
                         vegen.brokoli()
                         break
                     elif sayur == 2:
-                        vegen = vegetable(5, 200, 100)
+                        vegen = vegetable(4, 200, 100)
                         vegen.wortel()
                         break
                     elif sayur == 3:
-                        vegen = vegetable(4, 200, 100)
+                        vegen = vegetable(4, 180, 100)
                         vegen.kembang_kol()
                         break
                     elif sayur == 4:
-                        vegen = vegetable(3, 200, 100)
+                        vegen = vegetable(3, 160, 100)
                         vegen.bayam()
                         break
                     elif sayur == 5:
-                        vegen = vegetable(5, 200, 100)
+                        vegen = vegetable(4, 180, 100)
                         vegen.jagung_manis()
                         break
                     elif sayur == 6:
@@ -297,7 +350,7 @@ class Microwave(comp):
                         vegen.paprika()
                         break
                     elif sayur == 7:
-                        vegen = vegetable(4, 200, 100)
+                        vegen = vegetable(4, 180, 100)
                         vegen.zucchini()
                         break
             elif chioce == 6:
@@ -325,9 +378,19 @@ class Microwave(comp):
                         lobster.lobster()
                         break
             elif chioce == 7:
+                while True:
+                    code = int(input("Masukan Code barcode: "))
+                    food = incode(0,0,0,0,0)
+                    food.detect(code)
+                    confirm = input(f'{food.namefood}? (y/n): ').lower()
+                    if confirm == 'y':
+                        food.cook()
+            elif chioce == 8:
                 Microwave.display1()
                 print("Microwave telah dibersikan")
-            elif chioce == 0:
+            elif chioce == 99:
                 break
-
-Microwave.Start()       
+            elif chioce == 0:
+                print("Terima Kasih telah menggunakan produk kami hehehe")
+                break
+Microwave.Start()
